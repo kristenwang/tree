5c1fbe3ba5153bf9682312a7ab6cc5b7d75ff5ef
kristenwang <kristenwang23@gmail.com>
2016-10-02 16:20:58 -0400

    first try

--- pytree.py
+++ pytree.py
@@ -1,11 +1,61 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
 
 
 # YOUR CODE GOES here
+def printdir(path,padding,dir):
+    dir_num = len(dir)
+    dirs = os.listdir(path)
+    files=[]
+    for fi in dirs:
+        if not fi.startswith('.'):
+            files.append(fi)
+    file_num=len(files)
+    for i in range(file_num):
+        for k in range(dir_num):
+            if(files[i]==dir[k]):
+                print( padding+'├── ' + files[i])
+                printdir(path+os.sep+dir[k],padding+'│   ', dir[k:])
+            elif(i<k):
+            	print( padding+'├── ' + files[i])
+        if (i == file_num - 1):
+            print(padding+'└── ' + files[i])
+        elif(dir_num==1):
+            print(padding+ '├── ' + files[i])
+
+
+def tree(path,padding):
+    file_num = 0
+    files_n=[]
+    dirs_n=[]
+    root_n=[]
+    print('.')
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if not f.startswith('.'):
+                files_n.append(f)
+        for d in dirs:
+            if not d.startswith('.'):
+                dirs_n.append(d)
+    file_num=len(files_n)
+    dir_num = len(dirs_n)
+    printdir(path,padding,dirs_n)
+    print('')
+    print("%s directories, %s files" % (dir_num, file_num))
+
 
 
 if __name__ == '__main__':
     # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    #subprocess.run(['tree'] + sys.argv[1:])
+
+    if len(sys.argv)==1:
+    	path = os.getcwd()
+    	tree(path,'')
+    elif len(sys.argv)==2:
+        path = sys.argv[1]
+        tree(path,'')
+    else:
+    	print("Wrong Input")
